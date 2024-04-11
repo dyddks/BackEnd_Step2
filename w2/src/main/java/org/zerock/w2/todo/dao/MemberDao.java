f5ecfffc3a85dffb5c40e256f5cef28d0a6de018
dyddks <dyddks0115@naver.com>
2024-04-11 14:57:10 +0900

    feat: MemberDao 작성

---
+++ w2/src/main/java/org/zerock/w2/todo/dao/MemberDao.java
@@ -0,0 +1,32 @@
+package org.zerock.w2.todo.dao;
+
+import lombok.Cleanup;
+import org.zerock.w2.todo.domain.MemberVo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MemberDao {
+    public MemberVo getWithPassword(String mid, String mpw) throws Exception{
+        String sql = "select * from member where member_id=? and password=?";
+        MemberVo memberVo = null;
+
+        @Cleanup Connection conn = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement ps = conn.prepareStatement(sql);
+
+        ps.setString(1, mid);
+        ps.setString(2, mpw);
+        @Cleanup ResultSet rs = ps.executeQuery();
+
+        rs.next();
+
+        memberVo = MemberVo.builder()
+                .mid(rs.getString(1))
+                .mpw(rs.getString(2))
+                .mname(rs.getString(3))
+                .build();
+
+        return memberVo;
+    }
+}
