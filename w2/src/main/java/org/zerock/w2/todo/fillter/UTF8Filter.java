de87f5aadf52b2773c7963820dcdb5859176c19e
dyddks <dyddks0115@naver.com>
2024-04-11 14:38:33 +0900

    feat: utf8 인코딩 필터 작성

--- w2/src/main/java/org/zerock/w2/todo/fillter/UTF8Filter.java
+++ w2/src/main/java/org/zerock/w2/todo/fillter/UTF8Filter.java
@@ -1,2 +1,21 @@
-package org.zerock.w2.todo.fillter;public class UTF8Filter {
+package org.zerock.w2.todo.fillter;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/*"})
+@Log4j2
+public class UTF8Filter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("UTF8 Filter..............");
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        request.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
 }
