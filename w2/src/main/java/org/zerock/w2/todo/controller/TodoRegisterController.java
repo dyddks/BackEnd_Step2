f2a81eb9780df940f668aff3975819bd577319b8
dyddks <dyddks0115@naver.com>
2024-04-11 14:40:27 +0900

    style: 불필요한 라인 주석처리

--- w2/src/main/java/org/zerock/w2/todo/controller/TodoRegisterController.java
+++ w2/src/main/java/org/zerock/w2/todo/controller/TodoRegisterController.java
@@ -21,19 +21,19 @@ public class TodoRegisterController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("입력화면을 볼 수 있도록 구성");
 
-        HttpSession session = req.getSession();
-
-        if(session.isNew()) {
-            log.info("JSESSIONID 쿠키가 새로 만들어진 사용자");
-            resp.sendRedirect("/login");
-            return;
-        }
-
-        if(session.getAttribute("loginInfo") == null) {
-            log.info("로그인한 정보가 없는 사용자");
-            resp.sendRedirect("/login");
-            return;
-        }
+//        HttpSession session = req.getSession();
+//
+//        if(session.isNew()) {
+//            log.info("JSESSIONID 쿠키가 새로 만들어진 사용자");
+//            resp.sendRedirect("/login");
+//            return;
+//        }
+//
+//        if(session.getAttribute("loginInfo") == null) {
+//            log.info("로그인한 정보가 없는 사용자");
+//            resp.sendRedirect("/login");
+//            return;
+//        }
 
         // 입력 할 수있는 jsp로 forward
         // jsp에서는 다시 "/todo/register" post 포스트 요청을 보내면 DB에 등록
